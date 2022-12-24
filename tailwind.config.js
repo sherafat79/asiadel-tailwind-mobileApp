3369afbac3742d9896b50bce7380a41b69561973
mahyar <mohammadmahdisherafat79@gmail.com>
2022-12-24 12:55:59 +0330

    ..

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./index.html"],
+  theme: {
+    extend: {
+          colors: {
+            primary: '#5E32C0',
+           // secondary: '#ecc94b',
+     
+    },
+    },
+
+  },
+  plugins: [],
+}
